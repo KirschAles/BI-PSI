6842ca1d031d6c51150d78aa3dd600a0fb380b23
Ales Kirsch <kirschales@gmail.com>
2023-02-21 22:53:53 +0100

    Added test for veryfy client

--- hw/test/test_misc.py
+++ hw/test/test_misc.py
@@ -6,3 +6,8 @@ def test_hash():
     assert(main.calculate_hash(b'Mnau!') == 40784)
     assert(main.calculate_hash(b'') == 0)
     assert(main.calculate_hash(bytes('+ěýřáčíšěž=', encoding='utf-8')))
+
+
+def test_verify_client_key():
+    hash1 = main.calculate_hash(bytes('Oompa Loompa', encoding='utf-8'))
+    assert(main.verify_client_key(8389, 0, hash1))
